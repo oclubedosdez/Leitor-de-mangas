7c52999708fb9cca1ca00d8019f0d3cbb544fa81
DevJunior <criticossemdiploma@gmail.com>
2023-10-19 08:16:15 -0300

    manga adicionado

---
+++ mangas-e-manhaws/pages/mangas/Nihon e Youkoso Elf-san (Welcome to Japan, Elf-san)/capitulos/src/js/select.js
@@ -0,0 +1,24 @@
+var sele = document.getElementById("selecionar")
+
+
+    let select_element = `
+    <option value="./01.html">01</option>
+    <option value="./03.html">03</option>
+        <option value="./04.html">04</option>
+        <option value="./05.1.html">05.1</option>
+        <option value="./09.html">09</option>
+        <option value="./10.html">10</option>
+        <option value="./17.html">17</option>
+        <option value="./19.html">19</option>
+        <option value="./20.html">20</option>
+        <option value="./23.html">23</option>
+        <option value="./24.html">24</option>
+        <option value="./25.html">25</option>
+        <option value="./26.html">26</option>
+        <option value="./27.html">27</option>
+        <option value="./33.html">33</option>
+        <option value="./34.html">34</option>
+        <option value="./34.5.html">34.5</option>
+        <option value="./35.html">35</option>
+    `;
+    sele.innerHTML += select_element;
---
+++ mangas-e-manhaws/pages/mangas/modelo/script.js
@@ -0,0 +1,122 @@
+var box3 = document.querySelector(".box3")
+
+let capitulos = [
+    {
+        "id": "01",
+        "data" : "outubro 11, 2023"
+    },
+    {
+        "id": "02",
+        "data" : "outubro 6, 2023"
+    },
+    {
+        "id": "03",
+        "data" : "outubro 1, 2023"
+    },
+    {
+        "id": "04",
+        "data" : "outubro 13, 2023"
+    },
+    {
+        "id": "05",
+        "data" : "outubro 13, 2023"
+    },
+    {
+        "id": "06",
+        "data" : "outubro 13, 2023"
+    },
+    {
+        "id": "06.5",
+        "data" : "outubro 13, 2023"
+    },
+    {
+        "id": "07",
+        "data" : "outubro 13, 2023"
+    },
+    {
+        "id": "08",
+        "data" : "outubro 13, 2023"
+    },
+    {
+        "id": "09",
+        "data" : "outubro 13, 2023"
+    },
+    {
+        "id": "10",
+        "data" : "outubro 13, 2023"
+    },
+    {
+        "id": "11",
+        "data" : "outubro 13, 2023"
+    },
+    {
+        "id": "12",
+        "data" : "outubro 13, 2023"
+    },
+    {
+        "id": "12.5",
+        "data" : "outubro 13, 2023"
+    },
+    {
+        "id": "13",
+        "data" : "outubro 13, 2023"
+    },
+    {
+        "id": "14",
+        "data" : "outubro 13, 2023"
+    },
+    {
+        "id": "15",
+        "data" : "outubro 13, 2023"
+    },
+    {
+        "id": "15.5",
+        "data" : "outubro 13, 2023"
+    },
+    {
+        "id": "16",
+        "data" : "outubro 13, 2023"
+    },
+    {
+        "id": "17",
+        "data" : "outubro 13, 2023"
+    },
+    {
+        "id": "18",
+        "data" : "outubro 13, 2023"
+    },
+    {
+        "id": "18.5",
+        "data" : "outubro 13, 2023"
+    },
+    {
+        "id": "19",
+        "data" : "outubro 13, 2023"
+    },
+    {
+        "id": "20",
+        "data" : "outubro 13, 2023"
+    },
+    {
+        "id": "21",
+        "data" : "outubro 13, 2023"
+    },
+    {
+        "id": "22",
+        "data" : "outubro 13, 2023"
+    },
+];
+
+capitulos.forEach((banner) => {
+    let banner_element = `
+    <a class="capitulo" href="./capitulos/${banner.id}.html">
+            <img class="img-fluid w-25" src="https://github.com/oclubedosdez.png" alt="">
+            <div class="titulo_data">
+            <h1>Capitulo ${banner.id}</h1>
+            <h3>${banner.data}</h3> 
+            </div>
+        </a>
+    `;
+    box3.innerHTML += banner_element;
+})
+
