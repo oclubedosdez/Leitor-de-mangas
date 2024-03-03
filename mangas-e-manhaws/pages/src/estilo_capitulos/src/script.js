ba9c0625e7cc986ce5da81bd4002d13fddc17e41
DevJunior <criticossemdiploma@gmail.com>
2024-03-03 14:43:48 -0300

    atualizado em 03/03/2024

---
+++ mangas-e-manhaws/pages/src/estilo_capitulos/src/script.js
@@ -0,0 +1,341 @@
+let select_element = [];
+
+var inicia = 0;
+
+var nome_manga = nome;
+
+const select_custom = document.getElementById('selectOptions');
+
+// Usar o método find para buscar o num_capitulo com base no nome do manga (case insensitive)
+const nomeDoMangaBuscado = nome_manga;
+
+const mangaEncontrado = mangas_e_manhwas.find(manga => manga.nome.toLowerCase() === nomeDoMangaBuscado.toLowerCase());
+
+if (mangaEncontrado) {
+    const numCapitulos = mangaEncontrado.num_capitulos;
+    const textoCap = mangaEncontrado.text_cap;
+
+    for (let i = inicia; i <= numCapitulos; i++) {
+        select_element.push(`<div class='option' data-value='${i}'>capitulo ${i < 10 ? '0' : ''}${i}</div>`);
+    }
+
+} else {
+    console.log(`Manga não encontrado.`);
+}
+
+select_custom.innerHTML = select_element.join('');
+
+
+
+
+var modo_escuro = document.querySelector('.modo-claro');
+var botao = document.querySelector('.modo-claro')
+var cabecalho = document.querySelector('.titulo2')
+var navbar = document.querySelector('.navbar')
+
+// Adicione esta linha para verificar o estado do modo escuro no localStorage
+if (localStorage.getItem('modoEscuro') === 'ativo') {
+    document.body.classList.add('modo_black');
+    botao.innerHTML = 'Modo claro';
+}
+
+modo_escuro.addEventListener('click', function () {
+    // Alternar entre o modo claro e escuro
+    document.body.classList.toggle('modo_black');
+
+    // Atualizar o localStorage com o estado atual do modo escuro
+    if (document.body.classList.contains('modo_black')) {
+        localStorage.setItem('modoEscuro', 'ativo');
+        botao.innerHTML = 'Modo claro';
+    } else {
+        localStorage.setItem('modoEscuro', 'inativo');
+        botao.innerHTML = 'Modo escuro';
+
+        // Adicionar/remover outras classes ou estilos conforme necessário
+        cabecalho.classList.remove('modo_escuro');
+        botao.classList.remove('botao_escuro');
+        document.getElementById('logo').style.border = '2px solid white';
+    }
+});
+
+
+var titulo1 = document.querySelector('.titulo1');
+var titulo2 = document.querySelector('.titulo2');
+var nome = document.getElementById('nome_manga');
+
+
+
+
+
+var num_imgs = 80;
+
+
+
+
+
+
+
+if ('scrollBehavior' in document.documentElement.style === false) {
+    import('smoothscroll-polyfill').then(module => {
+        module.polyfill();
+    });
+}
+
+
+
+
+
+
+/*===============================================REDERIZA OS CAPITULOS DA PAGINA===================================================*/
+
+
+
+
+var capitulo = Number(localStorage.getItem('ValorCapitulo'))
+
+
+const mangaImages = [];
+let currentImageIndex = 0;
+
+const figura = document.createElement('img');
+
+
+
+
+
+
+
+
+const mangaImage = document.getElementById("manga-image");
+const previousBtn = document.getElementById("previous-btn");
+const nextBtn = document.getElementById("next-btn");
+const pageSelect = document.getElementById("page-select");
+const pageInfo = document.getElementById("page-info");
+var botao_proximo_capitulo = document.querySelectorAll(".botao-avancar-capitulo")
+var botao_voltar_capitulo = document.querySelectorAll(".botao-capitulo-anterior")
+const botoes_de_baixo = document.querySelector(".botões-de-baixo")
+const capitulo_selecionado = document.getElementById('cap_selecionado')
+
+
+
+var capitulo_fra = Math.trunc(capitulo)//caso o numero do capitulo seja fracionado;
+
+
+
+titulo1.innerHTML = `${nome_manga} - Cap.${capitulo < 10 ? "0" : ""}${capitulo}`
+nome.innerHTML = `${nome_manga} - Cap.${capitulo < 10 ? "0" : ""}${capitulo}`
+
+
+
+
+
+
+// console.log(select_element);
+
+
+botoes_de_baixo.classList.add('none')
+
+
+
+
+const container = document.getElementById('manga-container');
+const pageAberta = document.querySelector('.selecao-de-paginas');
+const seletorCapitulos = document.querySelector('.seletor-de-capitulos');
+
+
+
+
+
+var diretorio = `${caminho_pasta}${capitulo < 10 ? "0" : ""}${localStorage.getItem("ValorCapitulo")}`;
+
+
+
+
+botoes_de_baixo.classList.remove('none')
+
+
+
+   
+var imagensCarregadas = [];
+var imagensComErro = 0;
+
+// Adicione a classe 'no-scroll' ao corpo para desativar o scroll
+document.body.classList.add('no-scroll');
+container.classList.add('visibilidade')
+
+for (let i = 0; i <= num_imgs; i++) {
+    let img_manga = { 'id': i };
+
+    let manga_page = document.createElement('img');
+    manga_page.classList.add('page_cap');
+
+    manga_page.src = `${diretorio}/imgs/${img_manga.id}.jpg`;
+    manga_page.alt = '';
+
+    manga_page.onload = function () {
+        // A função será chamada se a imagem for carregada corretamente
+        imagensCarregadas.push(img_manga);
+        checkCarregamentoCompleto();
+        window.scrollTo(0, 0);
+    };
+
+    manga_page.onerror = function () {
+        // A função será chamada se a imagem não for carregada corretamente
+        // console.log(`A imagem ${img_manga.id} não foi carregada corretamente.`);
+        manga_page.classList.add('img_erro'); // Adiciona a classe 'img_erro'
+        imagensComErro++;
+
+        checkCarregamentoCompleto();
+        window.scrollTo(0, 0);
+    };
+
+    container.appendChild(manga_page);
+}
+
+function checkCarregamentoCompleto() {
+    // Verifica se todas as imagens foram processadas (carregadas ou com erro)
+    if (imagensCarregadas.length + imagensComErro === num_imgs) {
+        // console.log(`Número de imagens com erro: ${imagensComErro}`);
+        // Remova a classe 'no-scroll' do corpo para reativar o scroll
+        window.scrollTo(0, 0);
+        document.body.classList.remove('no-scroll');
+        container.classList.remove('visibilidade')
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*============================================= SELELECT CUSTOMIZADO E BOTOES DE IR E VOLTAR =======================================================*/
+const selectOptions = document.getElementById('selectOptions');
+
+function incializarselect() {
+    const selectHeader = document.getElementById('selectHeader');
+    const selectOptions = document.getElementById('selectOptions');
+    const options = document.querySelectorAll('.option');
+
+    // Função para adicionar a classe 'selected' e armazenar no localStorage
+    function selectOption(option) {
+        selectHeader.textContent = option.textContent;
+        selectOptions.style.display = 'none';
+
+        options.forEach(function (otherOption) {
+            otherOption.classList.remove('selected');
+        });
+
+        option.classList.add('selected');
+
+        const valorSelecionado = option.getAttribute('data-value');
+        localStorage.setItem('ValorCapitulo', valorSelecionado);
+    }
+
+
+
+    selectHeader.onclick = function () {
+        selectOptions.style.display = selectOptions.style.display === 'block' ? 'none' : 'block';
+    }
+
+
+    options.forEach(function (option) {
+        option.addEventListener('click', function () {
+            selectOption(option);
+            window.location.reload(false);
+        });
+    });
+
+
+
+    // console.log(`O valor armazenado é ${localStorage.getItem('ValorCapitulo')}`);
+
+    // Ao carregar a página, aplicar a classe 'selected' se existir um valor armazenado
+    const valorArmazenado = localStorage.getItem('ValorCapitulo');
+    if (valorArmazenado) {
+        const selectedOption = document.querySelector(`.option[data-value="${valorArmazenado}"]`);
+        if (selectedOption) {
+            selectOption(selectedOption);
+        }
+    }
+}
+
+
+incializarselect()
+
+
+
+
+document.addEventListener('click', function (event) {
+    const isClickInside = selectHeader.contains(event.target) || selectOptions.contains(event.target);
+    if (!isClickInside) {
+        selectOptions.style.display = 'none';
+    }
+});
+
+
+
+
+
+/*Botões*/
+botao_proximo_capitulo.forEach((avancar_capitulo) => {
+    avancar_capitulo.onclick = function () {
+        capitulo++;
+
+        titulo1.innerHTML = `${nome_manga} - Cap.${capitulo < 10 ? "0" : ""}${capitulo}`
+        nome.innerHTML = `${nome_manga} - Cap.${capitulo < 10 ? "0" : ""}${capitulo}`
+
+        localStorage.setItem('ValorCapitulo', capitulo)
+        window.location.reload(false);
+        window.scrollTo(0, 0);
+    }
+    if(capitulo_fra == select_element.length) {
+        avancar_capitulo.style.display = 'none'
+    }
+
+});
+
+
+
+botao_voltar_capitulo.forEach((capitulo_anterior) => {
+    capitulo_anterior.onclick = function () {
+        capitulo--;
+
+        titulo1.innerHTML = `${nome_manga} - Cap.${capitulo < 10 ? "0" : ""}${capitulo}`
+        nome.innerHTML = `${nome_manga} - Cap.${capitulo < 10 ? "0" : ""}${capitulo}`
+
+        localStorage.setItem('ValorCapitulo', capitulo)
+        window.location.reload(false);
+        window.scrollTo(0, 0);
+    }
+    if(capitulo_fra === inicia) {
+        capitulo_anterior.style.display = 'none'
+    }
+});
+
+
+// function pararCarregamento() {
+//     // Aguarda 5 segundos (5000 milissegundos) e depois cancela o carregamento
+//     setTimeout(function() {
+//       // Cancela o carregamento
+//       window.stop(); // Para navegadores modernos
+//       document.execCommand("Stop"); // Para navegadores mais antigos
+//     }, 5000); // 5000 milissegundos = 5 segundos
+//   }
+  
+//   // Chama a função para parar o carregamento após 5 segundos
+//   pararCarregamento();
+  
+
+
+
+
+
+
---
+++ mangas-e-manhaws/pages/src/script.js
@@ -0,0 +1,224 @@
+const nome_head = document.getElementById('nome_head');
+const nome_body = document.getElementById('nome_body');
+const manga_img_fundo = document.getElementById('manga_img_fundo');
+const tipo_quadrinho = document.getElementById('tipo_quadrinho');
+const imagen_principal = document.getElementById('imagen_principal');
+
+const generos = document.getElementById("generos");
+const autor = document.getElementById("autor");
+const artista = document.getElementById("artista");
+const alternativo = document.getElementById("alternativo");
+const estatus = document.getElementById("estatus");
+const sinopse = document.getElementById("sinopse");
+
+
+
+
+nome_head.innerText = nome;
+nome_body.innerText = nome;
+
+manga_img_fundo.src = caminho;
+imagen_principal.src = caminho;
+generos.innerHTML += `Genero(s): ${generos_link}`
+autor.innerHTML = autor_da_obra;
+artista.innerHTML = artista_da_obra;
+alternativo.innerHTML = `Alternativo: ${nome_alternativo}`;
+estatus.innerHTML = estatus_obra;
+sinopse.innerHTML = sinopese_da_obra;
+tipo_quadrinho.innerText = `Tipo: ${tipo}`
+
+
+/*RENDERIZAR CAPITULO NA TELA*/
+var box3 = document.querySelector(".box3");
+
+
+let capitulos = [];
+
+// Substitua esta linha pelo valor da data desejada (ano, mês, dia)
+const dataInicial = new Date(data); // Por exemplo, 30 de outubro de 2024
+
+// Função para formatar a data no formato desejado
+function formatData(data) {
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return data.toLocaleDateString("pt-BR", options);
+}
+
+// Usar o método find para buscar o num_capitulo com base no nome do manga (case insensitive)
+const nomeDoMangaBuscado = nome;
+
+const mangaEncontrado = mangas_e_manhwas.find(manga => manga.nome.toLowerCase() === nomeDoMangaBuscado.toLowerCase());
+
+if (mangaEncontrado) {
+    const numCapitulos = mangaEncontrado.num_capitulos;
+    const textoCap = mangaEncontrado.text_cap;
+    
+    const semanasPorCapitulo = 1; // Ajuste o valor conforme necessário
+
+    for (let i = capitulo_incia; i <= numCapitulos; i++) {
+        const intervaloEmSemanas = i * semanasPorCapitulo;
+        const dataAtual = new Date(dataInicial);
+        dataAtual.setDate(dataInicial.getDate() + 1); // Ajuste do dia para corrigir o problema de começar em 24 de fevereiro
+        dataAtual.setDate(dataAtual.getDate() + intervaloEmSemanas * 0.1 * i); // Multiplicamos por 2 para converter semanas em dias
+
+        capitulos.push({
+            "id": `${i < 10 ? `0${i}` : i}`,
+            "data": formatData(dataAtual),
+            "valor": `${i}`,
+        });
+    }
+
+    const capituloElements = capitulos.map((capitulo) => `
+        <a class="capitulo" href="./capitulos/index.html" data-valor="${capitulo.valor}">
+            <div class="titulo_data">
+                <h1>Capitulo ${capitulo.id}</h1> -
+                <h3>${capitulo.data}</h3> 
+            </div>
+            <img class="capitulo-img" src="https://raw.githubusercontent.com/oclubedosdez/Leitor-de-mangas/main/src/img/book.png" alt="">
+        </a>
+    `);
+
+    box3.innerHTML = capituloElements.join('');
+} else {
+    console.log(`Manga não encontrado.`);
+}
+
+
+
+
+
+const slider_mangas = document.querySelector('.owl-carousel');
+
+
+let carousel_mangas_items = [
+    {
+        "nome": "Atsumori-kun no Oyome-san",
+        "img": "Atsumori-kun no Oyome-san.jpg",
+    },
+
+    {
+        "tipo": "manhwa",
+        "nome": "Father, I Don't Want this Marriage! (Father, I Don't Want to Get Married!)",
+        "img": "Father, I Don't Want this Marriage! (Father, I Don't Want to Get Married!).jpg",
+        "pasta": "Knight of the Frozen Flower",
+        "estatus": "Sem capitulos",
+        "status": "completo",
+        "data": "2021",
+        "capitulos": "",
+    },
+    {
+        "tipo": "manhwa",
+        "nome": "My Childhood Friend Became an Obsessive Male Lead",
+        "img": "My Childhood Friend Became an Obsessive Male Lead.jpg",
+        "cor": "colorido",
+        "pasta": "My Childhood Friend Became an Obsessive Male Lead",
+        "estatus": "Capitulos: 46",
+        "status": "ativo",
+        "data": "2022"
+    },
+    {
+        "tipo": "manhwa",
+        "nome": "A Proprietária Bebê Está se Aposentando",
+        "img": "A Proprietária Bebê Está se Aposentando.jpg",
+        "pasta": "A Proprietária Bebê Está se Aposentando",
+        "estatus": "Sem capitulos",
+        "status": "ativo",
+        "data": "2022",
+    },
+    {
+        "tipo": "manga",
+        "nome": "Isekai De Tochi O Katte Noujou O Tsukurou",
+        "img": "Isekai De Tochi O Katte Noujou O Tsukurou.jpg",
+        "pasta": "Isekai De Tochi O Katte Noujou O Tsukurou",
+        "estatus": "Capitulos: 31",
+        "status": "ativo",
+        "data": "2019",
+    },
+
+    {
+        "tipo": "manga",
+        "nome": "Kekkon Yubiwa Monogatari",
+        "img": "Kekkon Yubiwa Monogatari.jpg",
+        "pasta": "Kekkon Yubiwa Monogatari",
+        "estatus": "Capitulos: 62",
+        "status": "ativo",
+        "data": "2016",
+    },
+
+    {
+        "tipo": "manhwa",
+        "nome": "Wojia Laopo Laizi Yiqian Nian Qian – My Wife Is From a Thousand Years Ago",
+        "img": "Wojia Laopo Laizi Yiqian Nian Qian – My Wife Is From a Thousand Years Ago.jpg",
+        "pasta": "Wojia Laopo Laizi Yiqian Nian Qian – My Wife Is From a Thousand Years Ago",
+        "estatus": "Capitulos: 31",
+        "status": "ativo",
+        "data": "2019",
+    },
+    {
+        "tipo": "manga",
+        "nome": "Fuufu Ijou, Koibito Miman",
+        "img": "Fuufu Ijou, Koibito Miman.jpg",
+        "pasta": "Fuufu Ijou, Koibito Miman",
+        "status": "ativo",
+    },
+    {
+        "tipo": "manhwa",
+        "nome": "Adelaide",
+        "img": "Adelaide.jpg",
+        "pasta": "Adelaide",
+        "estatus": "Sem capitulos",
+        "status": "completo",
+        "capitulos": "",
+    },
+];
+
+carousel_mangas_items.forEach((item) => {
+    // Procura o manga correspondente no array manga_info
+    let mangaInfo = mangas_e_manhwas.find(info => info.nome === item.nome);
+
+    // Se encontrar, adiciona a informação de número de capítulos
+    if (mangaInfo) {
+        item.capitulos = mangaInfo.num_capitulos;
+        item.text_cap = mangaInfo.text_cap;
+        item.estado = mangaInfo.estado;
+        item.lancamento = mangaInfo.lancamento;
+        item.tipo = mangaInfo.tipo;
+    }
+
+    let manga_item = `
+        <div class="item_manga">
+            <a href="#">
+                 <span class="${item.tipo}_carrocel"></span>
+                 <div class="manga_image">
+                <img src="../../../imgs/${item.img}" alt="">
+                </div>
+                <h1 title="${item.nome}">${item.nome}</h1>
+                <span class="ep">${item.lancamento}</span>
+            </a>
+                    
+         </div>
+    
+  `;
+
+    slider_mangas.innerHTML += manga_item;
+});
+
+
+
+var mangas_capitulos = document.querySelectorAll('.capitulo')
+
+
+mangas_capitulos.forEach((link) => {
+    link.onclick = function () {
+        const valorDoCapitulo = link.getAttribute('data-valor')
+
+        localStorage.setItem('ValorCapitulo', valorDoCapitulo)
+
+        
+
+       
+    }
+});
+
+
+
+
